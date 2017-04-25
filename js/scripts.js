063674db2703054d1784b46fb912c3f145eec237
Daniel Lopez and Jared Eiseman <daniel@>
2017-04-25 09:42:17 -0700

    add buttons to index.html, correct back link in contact.html, click events to change background color

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+  $(".btn-danger").click(function(){
+    $("body").removeClass();
+    $("body").addClass("red");
+  });
+
+  $(".btn-default").click(function(){
+    $("body").removeClass();
+  });
+
+});
